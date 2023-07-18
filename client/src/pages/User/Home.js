78d336dd7054df2ae41f95b437790ec4bc4f56c5
pankaj <2020csb1105@iitrpr.ac.in>
2023-07-19 00:33:10 +0530

    home page update

--- client/src/pages/User/Home.js
+++ client/src/pages/User/Home.js
@@ -1,44 +1,51 @@
-import React, {useState, useEffect} from 'react';
-import { myGifts } from '../../Services/APIs/UserAPI';
-import { addToCart } from '../../Services/APIs/UserAPI';
+import React, { useState, useEffect } from "react";
+import { myGifts } from "../../Services/APIs/UserAPI";
+import { addToCart } from "../../Services/APIs/UserAPI";
 import toast, { Toaster } from "react-hot-toast";
 
-
-const cartAdd = async(id) => {
-  //console.log(id)
+const cartAdd = async (id) => {
   const userMail = localStorage.getItem("userMail");
   const obj = {
     giftId: id,
-    userEmail: userMail
-  }
+    userEmail: userMail,
+  };
   try {
-    const res = await addToCart(obj)
-    if (res.status === 200 ) {
-      toast.success(res.data.message)
+    const res = await addToCart(obj);
+    if (res.status === 200) {
+      toast.success(res.data.message);
     } else {
-      toast.error("Error While Accessing Cart!")
+      toast.error("Error While Accessing Cart!");
     }
   } catch (error) {
-    toast.error(error)
+    toast.error(error);
   }
-  
-}
+};
+
 const GiftCard = ({ gift }) => {
-  
   return (
-    <div className="p-2 max-w-xs mx-auto bg-white shadow-lg rounded-lg overflow-hidden hover:bg-gray-100 hover:scale-105 transition-transform duration-300">
-      <img className="object-cover object-center h-56 w-full" src={gift.image} alt={gift.name} />
+    <div className="p-2 max-w-xs mx-auto bg-white shadow-lg rounded-lg overflow-hidden hover:bg-gray-100 hover:scale-105 transition-transform duration-300 dark:bg-gray-200">
+      <img
+        className="object-cover object-center h-56 w-full"
+        src={gift.image}
+        alt={gift.name}
+      />{" "}
       <div className="p-4">
-        <h2 className="text-gray-900 font-semibold text-lg">{gift.name}</h2>
-        <p className="mt-2 text-gray-600">{gift.description}</p>
+        <h2 className="text-gray-900 font-semibold text-lg"> {gift.name} </h2>{" "}
+        <p className="mt-2 text-gray-600"> {gift.description} </p>{" "}
         <div className="mt-4 flex items-center justify-between">
-          <span className="text-gray-900 font-bold">₹ {gift.price}</span>
-          <button className="px-3 py-1 bg-indigo-600 text-white font-semibold rounded hover:bg-indigo-700 active:bg-indigo-800 " onClick={() => cartAdd(gift._id)}>Add to Cart</button>
-        </div>
-      </div>
+          <span className="text-gray-900 font-bold"> ₹{gift.price} </span>{" "}
+          <button
+            className="px-3 py-1 bg-indigo-600 text-white font-semibold rounded hover:bg-indigo-700 active:bg-indigo-800"
+            onClick={() => cartAdd(gift._id)}
+          >
+            Add to Cart{" "}
+          </button>{" "}
+        </div>{" "}
+      </div>{" "}
     </div>
   );
 };
+
 const GiftShop = () => {
   const [gifts, setGifts] = useState([]);
 
@@ -55,42 +62,49 @@ const GiftShop = () => {
     getData();
   }, []);
 
-
   useEffect(() => {
     // Apply styles to the body element
     Object.assign(document.body.style, {
-      backgroundImage: 'url(/form1.avif)',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      backgroundAttachment: 'fixed',
-      minHeight: '100vh',
-      overflowY: 'auto'
-     
-      
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      backgroundAttachment: "fixed",
+      minHeight: "100vh",
+      overflowY: "auto",
     });
   }, []);
+
   return (
     <>
-    <div >
-      <Toaster/>
-      <div className='text-center px-5'><h1 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl dark:text-white bg-orange-500 rounded-lg p-1">
-        Discover The Perfect <mark className="px-2 text-yellow-200 bg-blue-600 rounded dark:bg-blue-500">Gift</mark> For Every <mark className="px-2 text-yellow-200 bg-blue-600 rounded dark:bg-blue-500">Special</mark> Moment
-        </h1></div>
-
-        <div className="py-12 p-4" style={{ maxWidth: '70vw', margin: '0 auto', height: '100%' }}>
-        
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {gifts.map((gift, index) => (
-            <GiftCard key={index} gift={gift} />
-          ))}
-        </div>
-      </div>
-
-
-    </div>
+      <div>
+        <Toaster />
+        <div className="text-center px-5">
+          <h1
+            className="mb-4 text-3xl font-extrabold leading-none tracking-tight md:text-4xl lg:text-5xl custom-heading"
+            style={{
+              fontFamily: "Lobster, cursive",
+              animation: "animate__zoomIn 1s",
+              // Add any other custom styles for the heading here
+            }}
+          >
+            Discover the perfect <span className="text-indigo-600"> Gift </span>
+            For Every <span className="text-red-500">
+              {" "}
+              special{" "}
+            </span> Moment{" "}
+          </h1>{" "}
+        </div>{" "}
+        <div className="py-12 px-4 md:px-8 lg:px-16">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {" "}
+            {gifts.map((gift, index) => (
+              <GiftCard key={index} gift={gift} />
+            ))}{" "}
+          </div>{" "}
+        </div>{" "}
+      </div>{" "}
     </>
   );
-}
+};
 
 export default GiftShop;
